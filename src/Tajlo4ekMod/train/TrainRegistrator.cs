9f59793bbf676f638fa9e0fa0375c98799701fe4
Tajlok <aleksey.belyaev.asd@gmail.com>
2025-12-28 19:28:42 +0300

    ref train

--- src/Tajlo4ekMod/train/TrainRegistrator.cs
+++ src/Tajlo4ekMod/train/TrainRegistrator.cs
@@ -1,53 +1,20 @@
 ﻿using Mafi;
 using Mafi.Base;
 using Mafi.Core.Mods;
-using Mafi.Core.Prototypes;
 using Mafi.Core.Trains;
-using System;
-using System.Reflection;
+using Tajlo4ekMod.utils;
 
 namespace Tajlo4ekMod.train
 {
     internal class TrainRegistrator : IModData
     {
+        private const int newCapacity = 500;
+
         public void RegisterData(ProtoRegistrator registrator)
         {
-            ChangeCapacity(registrator, Ids.Trains.WagonT1Unit, typeof(CargoWagonProto));
-            ChangeCapacity(registrator, Ids.Trains.WagonT1Fluid, typeof(CargoWagonProto));
-            ChangeCapacity(registrator, Ids.Trains.WagonT1Loose, typeof(CargoWagonProto));
-        }
-
-        private void SwitchValue(object obj, Type type, string fieldName, object newValue)
-        {
-            var field = type.GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
-            if (field != null)
-            {
-                try
-                {
-                    field.SetValue(obj, newValue);
-                }
-                catch (Exception ex)
-                {
-                    Logger.Log(ex.ToString());
-                    Logger.Log("error set " + fieldName);
-                }
-            }
-            else
-            {
-                Logger.Log("error get field " + fieldName);
-            }
-        }
-
-        private void ChangeCapacity(ProtoRegistrator registrator, Proto.ID id, Type type)
-        {
-            if (registrator.PrototypesDb.TryGetProto(id, out Proto obj))
-            {
-                SwitchValue(obj, type, "m_baseCapacity", 500.Quantity());
-            }
-            else
-            {
-                Logger.Log(id + " not found");
-            }
+            Utils.SwitchValue(registrator, Ids.Trains.WagonT1Unit, typeof(CargoWagonProto), "m_baseCapacity", newCapacity.Quantity());
+            Utils.SwitchValue(registrator, Ids.Trains.WagonT1Fluid, typeof(CargoWagonProto), "m_baseCapacity", newCapacity.Quantity());
+            Utils.SwitchValue(registrator, Ids.Trains.WagonT1Loose, typeof(CargoWagonProto), "m_baseCapacity", newCapacity.Quantity());
         }
     }
 }
---
+++ src/Tajlo4ekMod/utils/Utils.cs
@@ -0,0 +1,56 @@
+﻿using Mafi.Core.Mods;
+using System;
+using System.Reflection;
+using Mafi.Core.Prototypes;
+
+namespace Tajlo4ekMod.utils
+{
+    internal class Utils
+    {
+        public static void LogAllField(ProtoRegistrator registrator, Proto.ID id, Type type)
+        {
+
+            if (registrator.PrototypesDb.TryGetProto(id, out Proto obj) == false)
+            {
+                Logger.Log(id + " not found");
+                return;
+            }
+
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+            foreach (var field in fields)
+            {
+                Logger.Log(field.Name + " " + field.GetValue(obj));
+            }
+        }
+
+        public static void SwitchValue(ProtoRegistrator registrator, Proto.ID id, Type type, string fieldName, object newValue)
+        {
+            if (registrator.PrototypesDb.TryGetProto(id, out Proto obj) == false)
+            {
+                Logger.Log(id + " not found");
+                return;
+            }
+
+            var field = type.GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            if (field != null)
+            {
+                try
+                {
+                    field.SetValue(obj, newValue);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log(ex.ToString());
+                    Logger.Log("error set " + fieldName);
+                    return;
+                }
+            }
+            else
+            {
+                Logger.Log("error get field " + fieldName);
+                return;
+            }
+        }
+    }
+}
